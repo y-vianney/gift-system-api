e41482fd0236520ce5bee668f4ffdc424f8fa866
Yann A. V. ADJUMANY <69991917+y-vianney@users.noreply.github.com>
2025-12-19 12:37:47 +0000

    handle cors

--- api.py
+++ api.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from core import resolve_assignment
 
 
+origins = [
+    # "127.0.0.1:5500",
+    "*"
+]
+
 app = FastAPI(
     title='Gift System',
     version='1.0',
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Schemas
